79c80547f747067eaaacdfa53a63f2a04b19738b
Nightbug <nightbug@obakeconstructs.com>
2025-07-27 14:39:05 -0500

    Tweak Editor

--- scripts/editor.js
+++ scripts/editor.js
@@ -501,7 +501,7 @@ function create_picker_list() {
     } else {
       if (groupLetter !== "a" && groupLetter !== "e" && groupLetter !== "i" && groupLetter !== "o" && groupLetter !== "u") {
         if (subGroupLetter !== currentLetter2) {
-          picker.innerHTML += "<br /> \t"
+          picker.innerHTML += "<br />\t\t"
           subGroupLetter = currentLetter2;
         }
       }
