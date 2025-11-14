6ca28af108520eca745f616ad4618333ae540abd
Nightbug <nightbug@obakeconstructs.com>
2025-11-14 09:19:24 -0600

    updated image reviewer

--- scripts/reviewer.js
+++ scripts/reviewer.js
@@ -1,17 +1,17 @@
-const content = "https://raw.githubusercontent.com/ObakeConstructs/j-ono-data/main/";
-let deets = [];
-let sources = [];
-var col_size = 100;
+const CONTENT = "https://raw.githubuserCONTENT.com/ObakeConstructs/j-ono-data/main/";
+let RECORDS = [];
+let PUBLISHERS = [];
+var COLUMN_SIZE = 100;
 
 //=================================================================================
 
 async function opener() {
-  let response = await fetch(content + "json/j-ono-data.json");
-  deets = await response.json();
+  let response = await fetch(CONTENT + "json/j-ono-data.json");
+  RECORDS = await response.json();
   
-  const src = await fetch(content + "json/j-ono-source.json");
-  sources = await src.json();
-  sources = await sources.sort((a, b) => a.id < b.id ? -1 : 1);
+  const src = await fetch(CONTENT + "json/j-ono-source.json");
+  PUBLISHERS = await src.json();
+  PUBLISHERS = await PUBLISHERS.sort((a, b) => a.id < b.id ? -1 : 1);
   
   shower();
 }
@@ -23,27 +23,32 @@ function shower () {
   
   var w = document.documentElement.clientWidth;
   var across = (w/300).toFixed();
-  var col_size = 100/across;
-  //console.log(across, col_size);
+  var COLUMN_SIZE = 100/across;
   var cnt = 0;
   
   var row = document.createElement("div");
   row.setAttribute("class", "row");
   
-  for (var deetnum = 0; deetnum < deets.length; deetnum++) {
-    for (var defnum = 0; defnum < deets[deetnum].definition.length; defnum++) {
-      for (var exnum = 0; exnum < deets[deetnum].definition[defnum].example.length; exnum++) {
-        var src = deets[deetnum].definition[defnum].example[exnum].source;
-        var file = deets[deetnum].definition[defnum].example[exnum].file;
-        var disp = deets[deetnum].definition[defnum].example[exnum].display;
+  for (var deetnum = 0; deetnum < RECORDS.length; deetnum++) {
+    for (var defnum = 0; defnum < RECORDS[deetnum].definition.length; defnum++) {
+      for (var exnum = 0; exnum < RECORDS[deetnum].definition[defnum].example.length; exnum++) {
+        var src = RECORDS[deetnum].definition[defnum].example[exnum].source;
+        var file = RECORDS[deetnum].definition[defnum].example[exnum].file;
+        var disp = RECORDS[deetnum].definition[defnum].example[exnum].display;
+        var mean = RECORDS[deetnum].definition[defnum].meaning;
         
         var img = document.createElement("img");
         img.setAttribute("style", "width:100%; border: 1px solid black;");
-        img.setAttribute("src", content + "img/" + src + "/" + file);
+        img.setAttribute("src", CONTENT + "img/" + src + "/" + file);
+        
+        var textblock = document.createElement("div");
+        textblock.setAttribute("class", "text_block");
+        textblock.innerHTML = "<p><b>" + src + "/" + file + "</b><br />" + disp + "<br />" + mean + "</p>";
         
         var imgdiv = document.createElement("div");
-        imgdiv.setAttribute("style", "float:left; width:" + col_size + "%; padding: 5px 0px;");
-        imgdiv.innerHTML = src + "/" + file + "<br>" + disp;
+        imgdiv.setAttribute("style", "float:left; width:" + COLUMN_SIZE + "%; padding: 5px 0px;");
+        //imgdiv.innerHTML = src + "/" + file + "<br />" + disp + "<br />" + mean;
+        imgdiv.appendChild(textblock);
         imgdiv.appendChild(img);
         
         row.appendChild(imgdiv);
