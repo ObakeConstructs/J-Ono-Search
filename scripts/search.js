c92a2d660e0a6f77e120f6c9f0ac337395d78720
Nightbug <nightbug@obakeconstructs.com>
2025-11-16 15:19:41 -0600

    added dynamic romaji generation

--- scripts/search.js
+++ scripts/search.js
@@ -204,7 +204,7 @@ function shower_results(details, deet_num) {
   var rom = document.createElement("div");
   rom.setAttribute("class", "grid_main_block");
   details.romaji.forEach((itm) => {
-    if (rom.innerHTML.length > 0) rom.innerHTML += ", ";
+    if (rom.innerHTML.length > 0) rom.innerHTML += ",<br />";
     rom.innerHTML += itm;
   });
   
@@ -330,11 +330,81 @@ function show_stats() {
 
 //=================================================================================
 
+
+function get_romaji(kana) {
+  var multi_romaji = {
+    "きゃ":"kya","きゅ":"kyu","きょ":"kyo","ぎゃ":"gya","ぎゅ":"gyu","ぎょ":"gyo","しゃ":"sha","しゅ":"shu","しょ":"sho","じゃ":"ja","じゅ":"ju","じょ":"jo","ちゃ":"cha",
+    "ちゅ":"chu","ちょ":"cho","にゃ":"nya","にゅ":"nyu","にょ":"nyo","ひゃ":"hya","ひゅ":"hyu","ひょ":"hyo","みゃ":"mya","みゅ":"myu","みょ":"myo","りゃ":"rya",
+    "りゅ":"ryu","りょ":"ryo","キャ":"kya","キュ":"kyu","キョ":"kyo","ギャ":"gya","ギュ":"gyu","ギョ":"gyo","シャ":"sha","シュ":"shu","ショ":"sho","ジャ":"ja","ジュ":"ju",
+    "ジョ":"jo","チャ":"cha","チュ":"chu","チョ":"cho","ニャ":"nya","ニュ":"nyu","ニョ":"nyo","ヒャ":"hya","ヒュ":"hyu","ヒョ":"hyo","ミャ":"mya","ミュ":"myu","ミョ":"myo",
+    "リャ":"rya","リュ":"ryu","リョ":"ryo","ファ":"fa","フィ":"fi","フェ":"fe","フォ":"fo","フュ":"fyu","ティ":"ti","トゥ":"tu","ディ":"di","ドゥ":"du","チェ":"che",
+    "シェ":"she","ジェ":"je","ウィ":"wi","ウェ":"we","ウォ":"wo","ヴァ":"va","ヴィ":"vi","ヴェ":"ve","ヴォ":"vo"};
+  var single_romaji = {
+    "あ":"a","い":"i","う":"u","え":"e","お":"o","ア":"a","イ":"i","ウ":"u","エ":"e","オ":"o","か":"ka","き":"ki","く":"ku","け":"ke","こ":"ko","カ":"ka","キ":"ki",
+    "ク":"ku","ケ":"ke","コ":"ko","さ":"sa","し":"shi","す":"su","せ":"se","そ":"so","サ":"sa","シ":"shi","ス":"su","セ":"se","ソ":"so","た":"ta","ち":"chi",
+    "つ":"tsu","て":"te","と":"to","タ":"ta","チ":"chi","ツ":"tsu","テ":"te","ト":"to","な":"na","に":"ni","ぬ":"nu","ね":"ne","の":"no","ナ":"na","ニ":"ni",
+    "ヌ":"nu","ネ":"ne","ノ":"no","は":"ha","ひ":"hi","ふ":"fu","へ":"he","ほ":"ho","ハ":"ha","ヒ":"hi","フ":"fu","ヘ":"he","ホ":"ho","ま":"ma","み":"mi",
+    "む":"mu","め":"me","も":"mo","マ":"ma","ミ":"mi","ム":"mu","メ":"me","モ":"mo","や":"ya","ゆ":"yu","よ":"yo","ヤ":"ya","ユ":"yu","ヨ":"yo","ら":"ra","り":"ri",
+    "る":"ru","れ":"re","ろ":"ro","ラ":"ra","リ":"ri","ル":"ru","レ":"re","ロ":"ro","わ":"wa","を":"o","ん":"n","ワ":"wa","ヲ":"o","ン":"n","が":"ga","ぎ":"gi",
+    "ぐ":"gu","げ":"ge","ご":"go","ガ":"ga","ギ":"gi","グ":"gu","ゲ":"ge","ゴ":"go","ざ":"za","じ":"ji","ず":"zu","ぜ":"ze","ぞ":"zo","ザ":"za","ジ":"ji",
+    "ズ":"zu","ゼ":"ze","ゾ":"zo","だ":"da","ぢ":"ji","づ":"zu","で":"de","ど":"do","ダ":"da","ヂ":"ji","ヅ":"zu","デ":"de","ド":"do","ば":"ba","び":"bi",
+    "ぶ":"bu","べ":"be","ぼ":"bo","バ":"ba","ビ":"bi","ブ":"bu","ベ":"be","ボ":"bo","ぱ":"pa","ぴ":"pi","ぷ":"pu","ぺ":"pe","ぽ":"po","パ":"pa","ピ":"pi",
+    "プ":"pu","ペ":"pe","ポ":"po","ぁ":"a","ぃ":"i","ぅ":"u","ぇ":"e","ぉ":"o","ァ":"a","ィ":"i","ゥ":"u","ェ":"e","ォ":"o","ゃ":"ya","ゅ":"yu","ょ":"yo","ャ":"ya",
+    "ュ":"yu","ョ":"yo"," ":" "};
+    
+  var result = kana;
+  
+  // first pass - multi-kana
+  var result_length = result.length;
+  for (let i = 0; i < result_length; i++) {
+    let pair = result.slice(i, i + 2);
+    if (pair in multi_romaji) {
+      result = result.replace(pair, multi_romaji[pair]);
+      result_length = result.length;
+    }
+  }
+  
+  //second pass - single-kana
+  for (let i = 0; i < result_length; i += 1) {
+    let chr = result.slice(i, i + 1);
+    if (chr in single_romaji) {
+      result = result.replace(chr, single_romaji[chr]);
+      result_length = result.length;
+    }
+  }
+  
+  //third pass - remove little tsu
+  for (let i = 0; i < result_length; i += 1) {
+    let chr = result.slice(i, i + 1);
+    if (chr === "ッ" || chr === "っ") {
+      result = result.replace(chr, "");
+      result_length = result.length;
+    }
+  }
+  
+  //fourth pass - extended vowel
+  for (let i = 0; i < result_length; i += 1) {
+    let chr = result.slice(i, i + 1);
+    if (chr === "ー") {
+      var last_vowel = result.slice(i - 1, i)
+      if (last_vowel === "a" || last_vowel === "e" || last_vowel === "i" || last_vowel === "o" || last_vowel === "u") {
+        result = result.replace(chr, last_vowel);
+      }
+    }
+  }
+  
+  return result;
+
+}
+
+//=================================================================================
+
 function showPopup(img_path, title, src, contributor) {
   showOverlay();
   document.getElementById('popup').style.display = 'block';
   document.getElementById('popup_img').src = img_path;
   document.getElementById('imgTitle').innerHTML = title;
+  document.getElementById('imgSubTitle').innerHTML = get_romaji(title);
   var attribution = "Image used for education/instructional purposes only."
   var cont = "";  
   PUBLISHERS_ARRAY.forEach((pub) => {
@@ -502,6 +572,5 @@ async function prefetch() {
   const src = await fetch(content + "json/j-ono-source.json");
   PUBLISHERS_ARRAY = await src.json();  
   PUBLISHERS_ARRAY.sort(function(a, b){return a.publisher_name > b.publisher_name});
-  
 }
 
